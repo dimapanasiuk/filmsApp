1617399b1442062828551f96cd025a1669048c5c
dimapanasiuk <dimapanasiuk@icloud.com>
2020-07-27 15:49:33 +0300

    start work with header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,12 @@ import { Link, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { func } from 'prop-types';
 
+
 import { chooseCategory } from '../../redux/categoryData/categoryDataActions';
 import Search from '../Search/Search';
 
 import './header.scss';
+import { Button, Container, Paper, Tabs, Tab } from '@material-ui/core';
 
 
 const Header = ({ dispatch }) => {
@@ -15,33 +17,64 @@ const Header = ({ dispatch }) => {
     dispatch(chooseCategory(''));
   };
 
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+
   return (
     <header>
-      <nav className='header-navigation'>
+      <Paper>
+        <Container maxWidth='xl' className='header-layout'>
+          <nav className='header-navigation'>
+            <Tabs
+              onChange={handleChange}
+              value={value}
+              indicatorColor="primary"
+            >
+              <Tab value={0} label='Home' to='/' component={NavLink} />
+              <Tab value={1} label='Catalog' to='/categories' component={NavLink} />
+            </Tabs>
+            <ul className="main-header-right-block">
+              <li><Search /></li>
+              <li>
+                <Button variant="contained" color="primary" p={10}>
+                  <Link to='/login' className='login-button'>Login</Link>
+                </Button>
+              </li>
+            </ul>
+          </nav>
+        </Container>
+      </Paper>
 
-        <ul className="main-header-left-block">
-          <li>
-            <NavLink exact to="/"
-              className='header-navigation_default-button'
-            >Home</NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/categories"
-              className='header-navigation_default-button'
-              onClick={resetChooseCategory}
-            >Categories</NavLink>
-          </li>
-        </ul>
 
-        <ul className="main-header-right-block">
-          <li><Search /></li>
-          <li>
-            <Link to='/login' className='login-button'>Login</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+      {/* <nav className='header-navigation'>
+          <ul className="main-header-left-block">
+            <li>
+              <NavLink exact to="/"
+                className='header-navigation_default-button'
+              >Home</NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/categories"
+                className='header-navigation_default-button'
+                onClick={resetChooseCategory}
+              >Categories</NavLink>
+            </li>
+          </ul>
+          <ul className="main-header-right-block">
+            <li><Search /></li>
+            <li>
+              <Button variant="contained" color="primary">
+                <Link to='/login' className='login-button'>Login</Link>
+              </Button>
+            </li>
+          </ul>
+        </nav> */}
+    </header >
   );
 };
 
@@ -55,3 +88,4 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps)(Header);
+
