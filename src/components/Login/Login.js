af161a3403060038ca6190fab145287563c92b39
dimapanasiuk <dimapanasiuk@icloud.com>
2020-09-02 12:52:54 +0300

    feat: implementation pop up

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
 import Image from 'material-ui-image';
@@ -6,70 +6,87 @@ import { Typography } from '@material-ui/core';
 
 import './login.scss';
 
-const responseGoogle = (response) => {
-  let { givenName, familyName, email, imageUrl } = response.profileObj;
+let Login = () => {
+  const [userData, setUserData] = useState('');
 
-  let loginData = {
-    name: givenName,
-    secondName: familyName,
-    email: email,
-    image: imageUrl,
+  useEffect(() => {
+    const store = localStorage;
+    if (store.response !== undefined) {
+      setUserData(JSON.parse(store.response));
+    }
+  }, []);
+
+  const responseGoogle = (response) => {
+    setUserData(response);
+    localStorage.setItem('response', JSON.stringify(response));
   };
 
-  localStorage.setItem('loginData', JSON.stringify(loginData));
-};
+  const logout = () => {
+    setUserData('');
+    localStorage.clear();
+  };
 
-const logout = () => {
-  localStorage.clear();
-};
+  let loginContent = (data) => {
+    let cssLogIn;
+    let cssLogOut;
+    cssLogIn = data ? 'none' : 'block';
+    cssLogOut = data ? 'block' : 'none';
 
-let Login = () => {
-  // const isData = localStorage.hasOwnProperty('loginData');
-  let loginContent;
+    let name = '';
+    let imgUrl = '';
 
-  // if (isData) {
-  //   const content = JSON.parse(localStorage.loginData);
+    if (userData.profileObj !== undefined) {
+      name = userData.profileObj.name;
+      imgUrl = userData.profileObj.imageUrl;
+    }
 
-  //   loginContent = (
-  //     <div className="login-content">
-  //       <Image
-  //         src={content.image}
-  //         imageStyle={{ width: 'auto', height: 'auto' }}
-  //         disableSpinner
-  //       />
-  //       <Typography variant="h6" gutterBottom>
-  //         Hello {content.name} we can watch category
-  //       </Typography>
-  //       <GoogleLogout
-  //         clientId="218192214311-q84kv5i8v4lt8o4p27aa5c7lbqj2lruv.apps.googleusercontent.com"
-  //         buttonText="Logout"
-  //         onLogoutSuccess={logout}
-  //       ></GoogleLogout>
-  //     </div>
-  //   );
-  // // } else {
-  loginContent = (
-    <div className="login-content">
-      {/* <Typography variant="h6" gutterBottom>
-          login to see more
-        </Typography> */}
-      <GoogleLogin
-        clientId="218192214311-8j7mfaiej5hmcvtojt7n6u2q2tjhl675.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
-      <GoogleLogout
-        clientId="218192214311-8j7mfaiej5hmcvtojt7n6u2q2tjhl675.apps.googleusercontent.com"
-        buttonText="Logout"
-        onLogoutSuccess={logout}
-      ></GoogleLogout>
-    </div>
-  );
-  // }
+    return (
+      <>
+        <div style={{ display: cssLogIn, textAlign: 'center' }}>
+          <Typography
+            variant="h5"
+            gutterBottom
+            style={{ maxWidth: '300px', marginBottom: '40px' }}
+          >
+            Please login to make the {<b>category</b>} section active
+          </Typography>
+          <GoogleLogin
+            clientId="218192214311-8j7mfaiej5hmcvtojt7n6u2q2tjhl675.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
+        </div>
+        <div style={{ display: cssLogOut, textAlign: 'center' }}>
+          <Typography variant="h3" gutterBottom style={{ maxWidth: '300px' }}>
+            Hello
+          </Typography>
+          <Typography variant="h5" gutterBottom style={{ maxWidth: '300px' }}>
+            {name}
+          </Typography>
+          <Image
+            src={imgUrl}
+            imageStyle={{
+              width: 'auto',
+              height: '100%',
+              paddingBottom: '40px',
+            }}
+            disableSpinner
+          />
+          <div style={{ marginTop: '40px' }}>
+            <GoogleLogout
+              clientId="218192214311-8j7mfaiej5hmcvtojt7n6u2q2tjhl675.apps.googleusercontent.com"
+              buttonText="Logout"
+              onLogoutSuccess={logout}
+            ></GoogleLogout>
+          </div>
+        </div>
+      </>
+    );
+  };
 
-  return <div className="login">{loginContent}</div>;
+  return <div className="login">{loginContent(userData)}</div>;
 };
 
 export default Login;
