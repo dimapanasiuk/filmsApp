10810430fb3e727a2387a0eef2722d4c8b42ea8d
dimapanasiuk <dimapanasiuk@icloud.com>
2020-09-06 14:11:57 +0300

    fix: fix search bug

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+
 import { func, array } from 'prop-types';
 
 import { searchFilm } from '../../redux/searchData/searchDataActions';
@@ -9,70 +10,74 @@ import './search.scss';
 import { TextField, Button } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 
-class Search extends React.Component {
-  state = {
-    searchData: '',
+const Search = ({ films, searchFilm }) => {
+  const history = useHistory();
+
+  const [searchData, setSearchData] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const filmName = e.target.elements.name.value;
+    searchFilm(filmName);
+    history.push('/search');
   };
 
-  clickHandlerSearch = () => {
-    const { searchData } = this.state;
-    this.props.dispatch(searchFilm(searchData));
+  const clickHandlerSearch = () => {
+    searchFilm(searchData);
   };
 
-  changeHandler = (e) => {
-    this.setState({ searchData: e.t });
+  const changeHandler = (e) => {
+    setSearchData(e.t);
 
     const autoComplete = e.target.innerText;
     const textField = e.target.value;
 
     if (autoComplete) {
-      this.setState({ searchData: autoComplete });
+      setSearchData(autoComplete);
       return;
     } else {
-      this.setState({ searchData: textField });
+      setSearchData(textField);
     }
   };
 
-  render() {
-    let { films } = this.props;
-
-    return (
-      <form className="search">
-        <Autocomplete
-          id="custom-input-demo"
-          onChange={this.changeHandler}
-          options={films.map((option) => option.title)}
-          renderInput={(params) => (
-            <div ref={params.InputProps.ref}>
-              <TextField
-                size="small"
-                variant="outlined"
-                style={{ width: 200 }}
-                onChange={this.changeHandler}
-                label="Search"
-                type="text"
-                {...params.inputProps}
-              />
-            </div>
-          )}
-        />
-        <Button
-          className="search-button"
-          component={Link}
-          to="/search"
-          onClick={this.clickHandlerSearch}
-          variant="contained"
-          color="primary"
-        >
-          Search
-        </Button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className="search" onSubmit={submitHandler}>
+      <Autocomplete
+        id="custom-input-demo"
+        onChange={changeHandler}
+        options={films.map((option) => option.title)}
+        renderInput={(params) => (
+          <div ref={params.InputProps.ref}>
+            <TextField
+              name="name"
+              size="small"
+              variant="outlined"
+              style={{ width: 200 }}
+              onChange={changeHandler}
+              label="Search"
+              type="text"
+              {...params.inputProps}
+            />
+          </div>
+        )}
+      />
+      <Button
+        type="submit"
+        className="search-button"
+        component={Link}
+        to="/search"
+        onClick={clickHandlerSearch}
+        variant="contained"
+        color="primary"
+      >
+        Search
+      </Button>
+    </form>
+  );
+};
 
 Search.propTypes = {
-  dispatch: func,
+  searchFilm: func,
   films: array,
 };
 
@@ -80,4 +85,8 @@ const mapStateToProps = (state) => {
   return state;
 };
 
-export default connect(mapStateToProps)(Search);
+const mapDispatchToProps = {
+  searchFilm: searchFilm,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
